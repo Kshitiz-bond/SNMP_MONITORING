7090c6b8b24c4573d4a8a093d9ca2596886eb666
Kshitiz Agarwal <kshitiz.agarwal95@gmail.com>
2017-07-08 13:56:57 +0530

    Syntax for basic snmpget command

--- snmp_get.ps1
+++ snmp_get.ps1
@@ -1,7 +1,6 @@
-﻿$a = "14.20.16.50"
+﻿####how to get the basic value of parameter
 
-
-$d = $a
+$a = "14.20.16.50"
 
 Foreach ($i in $d)
 
@@ -9,3 +8,5 @@ Foreach ($i in $d)
 
 snmpget -Os -c public -v 1 $($i) 1.3.6.1.2.1.1.3.0 
 }
+
+####snmpwalk can also be used but snmpget is preferred.
